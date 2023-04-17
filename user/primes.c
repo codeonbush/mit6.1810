9de3bca27ce7f2680c5f767fb4dbf7e9e30b5ef2
codeonbush <codeonbush@foxmail.com>
2023-04-17 12:46:44 +0800

    fix

--- user/primes.c
+++ user/primes.c
@@ -36,12 +36,16 @@ int
 main(int argc, char *argv[]){
     int p[2];
     pipe(p);
-    for (int i = 2; i < MAXSIZE; i++){
-        write(p[1], &i, sizeof(int));
+    if(fork() == 0){
+        sleve_num(p);
+    }else{
+        // close(p[0]);
+        for (int i = 2; i < MAXSIZE; i++){
+            write(p[1], &i, sizeof(int));
+        }
+        close(p[1]);
+        wait(0);
     }
-    close(p[1]);
-    sleve_num(p);
-    wait(0);
     exit(0);
 }
 
