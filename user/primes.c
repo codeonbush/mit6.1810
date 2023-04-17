335d80ee83592e27133b2f9901cfa4572a526751
codeonbush <codeonbush@foxmail.com>
2023-04-17 12:19:32 +0800

    primes

---
+++ user/primes.c
@@ -0,0 +1,46 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+#define MAXSIZE  35
+
+int
+main(int argc, char *argv[]){
+    int p[2];
+    pipe(p);
+    for (int i = 2; i < MAXSIZE; i++){
+        write(p[1], &i, sizeof(int));
+    }
+    close(p[1]);
+    sleve(p);
+    exit(0);
+}
+
+
+int sleve(int *pl)
+{
+    int pr[2];
+    int num;
+    int prime;
+    
+    if (read(pl[0], &num, sizeof(int)) > 0){
+        prime = num;
+        printf("prime %d\n", prime);
+        pipe(pr);
+
+        if(fork() == 0){
+            sleve(pr);
+        }else{
+            close(pr[0]);
+            while (read(pl[0], &num, sizeof(int)) > 0){
+                if(num % prime != 0){
+                    write(pr[1], &num, sizeof(int));
+                }
+            }
+            close(pl[0]);
+            close(pr[1]);
+            wait(0);
+        }
+    }
+    exit(0);
+}
